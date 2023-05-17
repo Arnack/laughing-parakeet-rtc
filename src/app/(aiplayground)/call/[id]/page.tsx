c41052add775c4f8f1097e751f2f691264df8586
grigorii-o <grigorii@authentise.com>
2023-05-17 18:33:53 +0100

    Fix: Call comp refactered

--- src/app/(aiplayground)/call/[id]/page.tsx
+++ src/app/(aiplayground)/call/[id]/page.tsx
@@ -2,11 +2,11 @@
 
 import VideoCall from "./_components/VideoCall2";
 import { Box } from "@chakra-ui/react";
-import { auth } from '@/service/firebase/firebaseConfig';
 
 const CallPage = () => {
 
   const callId = location.pathname.split("/")[2];
+  
   return (
     <Box>
       <VideoCall callId={callId} />
--- src/app/(aiplayground)/call/page.tsx
+++ src/app/(aiplayground)/call/page.tsx
@@ -2,11 +2,17 @@
 
 import { Box } from "@chakra-ui/react";
 import { useAuth } from "@/authContext";
+import Link from "next/link";
 
 const CallPage = () => {
+  const { user } = useAuth();
+
+  console.log('user>>>', user);
   return (
     <Box>
-      
+      <Link href={`/call/${user?.uid}`}>
+        Call
+      </Link>
     </Box>
   );
 };
